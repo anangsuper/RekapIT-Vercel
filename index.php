ae691baac47a83d71d77a56685b427e8bc5c4e9d
akhmadhafiz <akhmadhafiz2016@gmail.com>
2026-06-20 23:10:31 +0800

    Fix: Memuat auth helper sebelum header untuk menghindari error undefined function

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 ob_start();
 session_start();
 require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/helpers/auth.php'; // Memastikan helper dimuat sebelum header
 
 if (!isset($_SESSION['user_id'])) {
     $currentPage = basename($_SERVER['PHP_SELF']);
